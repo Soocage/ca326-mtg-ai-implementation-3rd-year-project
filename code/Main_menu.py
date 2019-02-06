24b53723f5b1d6f1a6da3dcbc1bfc73b6aa89920
Kacper Slowikowski <kacper.slowikowski2@mail.dcu.ie>
2019-02-06 12:26:38 +0000

    Minor changes to quit function. Added more to the menu screen.

--- code/Main_menu.py
+++ code/Main_menu.py
@@ -3,6 +3,7 @@ import time
 
 #initializing pygame module
 pygame.init()
+pygame.mixer.init()
 
 #make the window and taking dimentions of your display
 display_info = pygame.display.Info()
@@ -10,15 +11,24 @@ display_size =(display_width, display_height)= (display_info.current_w, display_
 gameDisplay = pygame.display.set_mode((display_size))
 
 #define button dimentions
-button_length = (display_width/5)
+button_length = (display_width/4)
 button_height = (display_height/8)
 
 #define button orientation
 start_game_button_x = (display_width/2)- (button_length + (display_width/10))
-start_game_button_y = (display_height/3)*2
+start_game_button_y = (display_height/2.5)
 
-exit_game_button_x = (display_width/2) + (display_width/10)
-exit_game_button_y = (display_height/3)*2
+login_button_x = (display_width/2) + (display_width/10)
+login_button_y= (display_height/2.5)
+
+options_button_x = (display_width/2)- (button_length + (display_width/10))
+options_button_y = (display_height/3)*1.75
+
+deck_button_x = (display_width/2) + (display_width/10)
+deck_button_y = (display_height/3)*1.75
+
+exit_game_button_x = (display_width/2) - (button_length/2)
+exit_game_button_y = (display_height/3)*2.25
 
 #colors
 button_blue = (125,137,240)
@@ -32,21 +42,21 @@ bright_red = (255,0,0)
 bright_green = (0,255,0)
 blue = (0,0,255)
 
+bg_color = grey
 
 
+#Creates the name of the window and initiates how were timing changes in the window
 pygame.display.set_caption("Magic The Gathering")
 clock = pygame.time.Clock()
 
-bg = pygame.image.load("Menu_background.png")
-bg = pygame.transform.scale(bg, display_size)
+cursor = pygame.image.load("cursor.png")
+cursor = pygame.transform.scale(cursor, (200, 200))
 
+#loads in the logo of the game and scales the image to appropriate size
 logo = pygame.image.load("logo.png")
 logo_width = int(display_width/2)
 logo_height = int(display_height/4)
 logo = pygame.transform.scale(logo, (logo_width, logo_height))
-
-cardImage = pygame.image.load("cursor.png")
-cardImage = pygame.transform.scale(cardImage, (200, 200))
 
 
 x = (display_width * 0.4)
@@ -73,8 +83,8 @@ def lose():
     message_display("goodbye")
 
 
-def cursor(x,y):
-    gameDisplay.blit(cardImage, (x,y))
+def cursor_pos(x,y):
+    gameDisplay.blit(cursor, (x,y))
 
 def game_loop():
     running = True
@@ -87,74 +97,136 @@ def game_loop():
                 pygame.quit()
                 quit()
                 
-                
+            if event.type == pygame.KEYDOWN:                
 
                 if event.key == pygame.K_P:
-                    lose()
+                    lose() 
+
+                if event.key == pygame.K_ESCAPE:
+                    myquit()
                     
-        gameDisplay.blit(bg, (0,0))
-        cursor(mx,my)
+        cursor_pos(mx,my)
         pygame.display.update()
         clock.tick(60)
         
 #This will create buttons an position them
 def button(msg,x,y,w,h,ic,ac,action = None):
+    
     #Take in the co-ordinates of the mouse
     mouse = pygame.mouse.get_pos()
+    
     #Check what buttons have been pressed by the mouse
     click = pygame.mouse.get_pressed()
+    
     #Check if mouse position is within the boundry of the button
     if (x + w) > mouse[0] > x and (y + h) > mouse[1] > y:
+        
         #Highlight the button with a lighter shad of blue
         pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
         if click[0] == 1 and action != None:
+            
             #If we clicked on play launch the game_loop
-            if action == "play":
+            if action == "play_vs_ai":
                 game_loop()
-            #If we clicked quit run the exit function
+                
+            #If we clicked quit run the exit function   
             elif action == "quit":
-                pygame.quit()
-                quit()
+                myquit()
+
+            #run the options function
+            elif action == "options":
+                options()
+
+            #run the login function
+            elif action == "login":
+                login()
+                
     #Else we are not over the button so it stays as it is
     else:
         pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+        
     #This will display whatever message we provided in freesansbold on the centre of the button
-    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    smallText = pygame.font.Font("freesansbold.ttf", 40)
     textSurf, textRect = text_object(msg, smallText)
     textRect.center = ((x +(w/2)), (y + (h/2)))
     gameDisplay.blit(textSurf, textRect)
+    
         
 
+
+        
+
+        
+        
+
+        
+        
+
+        
+    
 #This will run the initial screen you will see before you get to the game screen
 def game_intro():
-
-    intro = True
-
-    while intro:
+    in_intro = True
+    while in_intro:
+        (mx, my) = pygame.mouse.get_pos()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-                
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    quit()
+                myquit()
 
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_ESCAPE:
+                    myquit()
 
         #preparing background
-        gameDisplay.fill(grey)
-        
+        gameDisplay.fill(bg_color)
+
         #preparing logo
-        gameDisplay.blit(logo, ((display_width/2 -(logo_width/2)),(display_height/8)))
-        
+        gameDisplay.blit(logo, ((display_width/2 -(logo_width/2)),(display_height/15)))
+
         #creating buttons
-        button("start",start_game_button_x, start_game_button_y, button_length, button_height, button_blue, hover_button_blue, "play")
-        button("End", exit_game_button_x, exit_game_button_y, button_length, button_height, button_blue, hover_button_blue, "quit")
+        button("Fight Vs Ai",start_game_button_x, start_game_button_y, button_length, button_height, button_blue, hover_button_blue, "play_vs_ai")
+        button("Quit", exit_game_button_x, exit_game_button_y, button_length, button_height, button_blue, hover_button_blue, "quit")
+        button("Options",options_button_x, options_button_y, button_length, button_height, button_blue, hover_button_blue, "options")
+        button("Deck Tools", deck_button_x, deck_button_y, button_length, button_height, button_blue, hover_button_blue, "deck_tools")
+        button("Login",login_button_x, login_button_y, button_length, button_height, button_blue, hover_button_blue, "login")
+        
+
+        cursor_pos(mx, my)
 
         #printing to screen
         pygame.display.update()
-        clock.tick(5)
+
+def options():
+    in_options = True
+    while in_options:
+        (mx, my) = pygame.mouse.get_pos()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                myquit()
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_ESCAPE:
+                    myquit()
+
+        gameDisplay.fill(bg_color)
+        
+        pygame.draw.rect(gameDisplay, black, ((display_width/6) -10, (display_height/6)-10, ((display_width/3)*2)+20, ((display_height/3)*2)+20))
+        pygame.draw.rect(gameDisplay, bright_red, (display_width/6, display_height/6, (display_width/3)*2, (display_height/3)*2))
+        button("Change resolution",start_game_button_x, start_game_button_y, button_length, button_height, button_blue, hover_button_blue, "change resolution")
+        button("Quit", exit_game_button_x, exit_game_button_y, button_length, button_height, button_blue, hover_button_blue, "quit")
+        button("+",options_button_x, options_button_y, button_length, button_height, button_blue, hover_button_blue, "options")
+        button("-",options_button_x, options_button_y, button_length, button_height, button_blue, hover_button_blue, "options")
+        button("Background color",options_button_x, options_button_y, button_length, button_height, button_blue, hover_button_blue, "options")
+        cursor_pos(mx,my)
+        pygame.display.update()
+
+def myquit():
+    pygame.quit()
+    quit()
 
 game_intro()
 pygame.quit()
