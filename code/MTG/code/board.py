f3edd734157e6de68f20770f74a0043e89eb31d3
mccans32 <sean.mccann32@mail.dcu.ie>
2019-02-22 13:29:22 +0000

    working on draw search land function

--- code/MTG/code/board.py
+++ code/MTG/code/board.py
@@ -213,6 +213,7 @@ class Board():
             PLAYER_2_BATTLEFIELD_SPRITE_CARD_GROUP.draw(self.display)
 
     def draw_search_land(self, player):
+        LAND_CARD_SPRITE_GROUP = pygame.sprite.Group()
         pygame.mouse.set_visible(True)
         time.sleep(1)
         (display_width, display_height) = self.display_size
@@ -224,61 +225,89 @@ class Board():
         draw_search_box_h = ((display_height)*(18/20))
         draw_search_box_w = ((display_width)*(18/20))
 
+        padding_w = (self.draw_search_box_w/11)/11
+        upper_padding_h = (self.draw_search_box_h/7)/7
+        card_h = (self.draw_search_box_h/7)
+        card_w = card_h*(63/88)
+
         in_draw_search = True
-        while in_draw_search:
-            for event in pygame.event.get():
-                pos = pygame.mouse.get_pos()
-                mx, my = pos[0], pos[1]
+        land_cards = []
+        for card in player.deck.cards:
+            if card.card_type == "Land":
+                land_cards.append(card)
 
-                if event.type == pygame.QUIT:
-                    myquit()
+        pygame.draw.rect(self.display, (255,255,255), (draw_search_box_x, draw_search_box_y, draw_search_box_w, draw_search_box_h))
+ 
+        row_1 = []
+        row_2 = []
+        row_3 = []
+        row_4 = []
+        row_5 = []
+        row_6 = []
+        for i in range(len(land_cards)):
+            if i < 10:
+                row_1.append(land_cards[i])
+            elif 10 < i < 20:
+                row_2.append(land_cards[i])
+            elif 20 < i < 30:
+                row_3.append(land_cards[i])
+            elif 30 < i < 40:
+                row_4.append(land_cards[i])
+            elif 40 < i < 50:
+                row_5.append(land_cards[i])
+            elif 50 < i < 60:
+                row_6.append(land_cards[i])
 
-                if event.type == pygame.KEYDOWN:
+        
+        i = 0
+        while i < len(row_1):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h)
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
+        i = 0
+        while i < len(row_2):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h*2) + card_h 
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
+        i = 0
+        while i < len(row_3):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h*3) + (card_h*2)
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
+        i = 0
+        while i < len(row_4):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h*4) + (card_h*3)
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
+        i = 0
+        while i < len(row_5):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h*5) + (card_h*4)
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
+        i = 0
+        while i < len(row_6):
+            land_card = row_1[i]
+            x = (padding_w*i+1) + (card_w*i)
+            y = (upper_padding_h*5) + (card_h*4)
+            land_sprite = CardSprite(row_1[i], x, y, card_w, card_h)
+            LAND_CARD_SPRITE_GROUP.ADD(land_sprite)
 
-                    if event.key == pygame.K_ESCAPE:
-                        myquit()
+        elif player == "AI":
+            return
 
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        return
+        LAND_CARD_SPRITE_GROUP.draw()
 
-            pygame.draw.rect(self.display, (255,255,255), (draw_search_box_x, draw_search_box_y, draw_search_box_w, draw_search_box_h))
-            if player == "player":
-                land_cards = []
-                for card in player.deck.cards:
-                    if card.card_type == "Land":
-                        land_cards.append(card)
-                row_1 = []
-                row_2 = []
-                row_3 = []
-                row_4 = []
-                row_5 = []
-                row_6 = []
-                for i in range (len(land_cards)):
-                    if i < 10:
-                        row_1.append(land_cards[i])
-                    elif 10 < i < 20:
-                        row_2.append(land_cards[i])
-                    elif 20 < i < 30:
-                        row_3.append(land_cards[i])
-                    elif 30 < i < 40:
-                        row_4.append(land_cards[i])
-                    elif 40 < i < 50:
-                        row_5.append(land_cards[i])
-                    elif 50 < i < 60:
-                        row_6.append(land_cards[i])
-
-                padding_w = (self.player_2_battlefield_box.w/16)/16
-                card_h = (self.player_2_battlefield_box.h/10)*8
-                card_w = draw_search_box_w / 11
-
-
-
-
-            elif player == "AI":
-                return
-
-            pygame.display.update()
+        pygame.display.update()
 
 
     def view_card(self, card):
